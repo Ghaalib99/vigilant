521adde00c5446d7b1ee9f24fb2d010fd032ffb5
ghaalib <olaolajide99@gmail.com>
2025-03-07 18:28:18 +0100

    deployment test 3

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Dashboard from "../screens/dashboard/Dashboard";
+
+const page = () => {
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
+};
+
+export default page;
