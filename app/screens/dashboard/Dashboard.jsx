44f68365dcd29bc3c838fdfe95f8059d64ab2920
ebuberoderick <ebuberoderick2@gmail.com>
2025-03-18 10:30:21 +0000

    set links

--- app/screens/dashboard/Dashboard.jsx
+++ app/screens/dashboard/Dashboard.jsx
@@ -10,28 +10,28 @@ const Dashboard = () => {
     {
       title: "Total Users",
       description: "All users of the platform",
-      link: "/",
+      link: "/dashboard",
       icon: 32,
       bg: "bg-[#46010240]"
     },
     {
       title: "Newly Joined Members",
       description: "Summary of newly joined members",
-      link: "/",
+      link: "/dashboard",
       icon: 432,
       bg: "bg-[#81AB2740]"
     },
     {
       title: "Reported Issues",
       description: "Issues that have been reported",
-      link: "/",
+      link: "/dashboard",
       icon: 21,
       bg: "bg-[#C1DDC340]"
     },
     {
       title: "Resolved Issues",
       description: "Issues that have not been resolved",
-      link: "/",
+      link: "/dashboard",
       icon: 342,
       bg: "bg-[#F88C2940]"
     },
@@ -51,7 +51,7 @@ const Dashboard = () => {
       <div className="grid grid-cols-1 md:grid-cols-3">
         <div className="pb-2 col-span-2 shadow-md rounded-lg">
           <div className="flex relative items-center px-6">
-            <div className="flex-grow">Your Progress Summary</div>
+            <div className="flex-grow">Incidents Summary</div>
             <div onMouseLeave={() => setShowOption(false)} onClick={() => setShowOption(!showOption)} className="flex  relative border rounded-lg px-3 py-1 bg-white">
               <div className="pb-0 font-semibold flex items-center gap-6 cursor-pointer">{option} </div>
               <div className={`absolute left-0 top-10 shadow-md border z-40 bg-white rounded-lg p-1 ${!showOption && "hidden"}`}>
